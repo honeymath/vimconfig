4f05a351998cf1ef8c423d1900d67bde8f92a99f
honeymath <qqzipcar@gmail.com>
2025-07-06 13:48:49 -0400

    updated

--- tools/parser.py
+++ tools/parser.py
@@ -3,36 +3,48 @@ from collections import defaultdict
 import json
 # 🌟 Node 数据结构
 class Node:
-    def __init__(self, type="", content=None, metadata=None):
+    def __init__(self, type="", content=[], metadata={}):
         self.type = type
         self.children = []
         self.content = content if content else []
         self.metadata = defaultdict(list)
         if metadata:
             self.add_metadata(metadata)
-        if self.type != 'context':
-            self.new_context_child()
 
     def reverse(self):
         self.children.reverse()
-        self.metadata = {k: v[::-1] for k, v in self.metadata.items()}
+        self.content.reverse()
+        for key in self.metadata:
+            self.metadata[key].reverse()
         for child in self.children:
             child.reverse()
 
     def to_dict(self):
-        return {
-            'type': self.type,
-            'content': self.content,
-            'metadata': dict(self.metadata),
-            'children': [child.to_dict() for child in self.children]
-        }
+        fucker = {}
+        if self.type:
+            fucker['type']= self.type
+        if self.content:
+            fucker['content'] = self.content
+        if self.children:
+            fucker['children'] = [child.to_dict() for child in self.children]
+        for k, v in self.metadata.items():
+            if v and v[0]:
+                fucker[f'm.{k}'] = v[-1]
+        return fucker
 
     def to_json(self, indent=2):
         return json.dumps(self.to_dict(), indent=indent)
     
     def add_metadata(self, metadata):
-        for k, v in metadata.items():
-            self.metadata[k].append(v)
+        if not isinstance(self.metadata, defaultdict):
+            print("YOU SUPER FUCKER!!! ")
+        try:
+            for k, v in metadata.items():
+                self.metadata[k].append(v)
+        except Exception as e:
+            print("Fuck the whole world!")
+            print("Is default faught:", isinstance(self.metadata,defaultdict))
+            raise e
 
     def new_context_child(self, metadata=None, type='context'):
         child = Node(type=type, metadata=metadata)
@@ -60,8 +72,10 @@ class Node:
             return
 #            raise Exception('who is your daddy!')
         self.children[-1].content.extend(child.content)
-        for k, v_list in child.metadata.items():
-            self.children[-1].metadata[k].extend(v_list)
+        super_fucker = {rinima:bi for rinima,bi in child.metadata.items()}
+        self.children[-1].add_metadata(super_fucker)
+#        for k, v_list in child.metadata.items():
+#            self.children[-1].metadata[k].extend(v_list)
 
     def kidnap_children(self, children):
         self.children.extend(children)
@@ -163,11 +177,13 @@ class Parser:
             self.stack[-1].new_context_child(metadata={type: metadata})
         elif self.state == 'watch':
             self.stack.append(self.stack[-1].new_non_context_child(type='watch'))
+            self.stack[-1].new_context_child() ### added for fogic
             self.stack[-1].children[-1].add_metadata({type: metadata})
         elif self.state == old_state:
             self.stack[-1].new_context_child(metadata={type: metadata})
         else:
             self.stack.append(self.stack[-1].new_non_context_child(type=self.state))
+            self.stack[-1].new_context_child() ### added for fogic
             self.stack[-1].children[-1].add_metadata({type: metadata})
 
     def reverse_handle_block_match(self, type, metadata, line):
@@ -178,6 +194,7 @@ class Parser:
         #from now on, the selfstate is the current type
         if self.state == 'end': ## it has ignore the previous fuck
             self.stack.append(self.stack[-1].new_non_context_child(type='end', metadata={}))
+            self.stack[-1].new_context_child() ### added for fogic
             return
         elif self.state == 'watch':
             self.stack[-1].type = self.state
@@ -242,7 +259,7 @@ class Parser:
             if self.stack[-1].children:
                 self.stack[-1].children[-1].content.append(line)
             else:
-                self.stack[-1].content.append(line)
+                raise Exception("FUCKER your mother!")
         elif TYPE == "BLOCK":
             if self.mode == 'reverse':
                 self.reverse_handle_block_match(type_, metadata[type_], line)
@@ -262,9 +279,12 @@ if __name__ == '__main__':
     parser.set_syntax_chars(comment_char='#', escape_char='##')
     resrap.set_syntax_chars(comment_char='#', escape_char='##')
     test_cases = [
-        "#ai:",
-        "#see:",
-        "#ai:",
+        "tag0",
+        "#ai:1",
+        "tag1",
+        "#see:2",
+        "tag2",
+        "#ai:3",
         "fuck0",
         "fuck1",
         "fuck2",
@@ -276,12 +296,16 @@ if __name__ == '__main__':
         "fuck8",
         "fuck9",
         "#end",
+        "rinima1",
         "#end",
-        "#end"
+        "rinima2",
+        "#end",
+        "rinima3",
     ]
 #    test_cases = test_cases[::-1]  # Reverse the test cases for reverse mode
 
-    cursor = 5
+    cursor = 9
+    parser.stack[-1].new_context_child(metadata={})  # This is SOF the init line mother fucker!!!!
     for i, line in enumerate(test_cases[cursor:]):
 #        print(f"Test {i}: {line}")
         TYPE, type_, metadata, restline = parser.parse(line)
@@ -290,9 +314,10 @@ if __name__ == '__main__':
 #        print(f"  Current node type: {parser.stack[-1].type}")
 #        print("-" * 40)
 #    parser.reverse_handle_block_match('', {}, '')# this is the init line
+    resrap.stack[-1].new_context_child(metadata={})  # This is EOF. this is glue structure.
     for i, line in enumerate(test_cases[cursor-1::-1]):
         TYPE, type_, metadata, restline = resrap.parse(line)
-    resrap.reverse_handle_block_match('', {}, '')  # this is the init line
+    resrap.reverse_handle_block_match('', {}, '')  # this is SOF the init line
 #    if (l:=parser.stack.len() >=0):
 #        print(parser.stack[-l].to_json(indent=2))
 #    else:
@@ -303,18 +328,49 @@ if __name__ == '__main__':
     future = parser.stack._history
     freedom = None
     counter = 0
-    for regret,hope in list(zip(history,future))[::-1]:
+    for ind, regret in enumerate(history):
+        print(f"History {ind}: {regret.to_json(indent=2)}")
+        print("-" * 40)
+
+    for ind, hope in enumerate(future):
+        print(f"Future {ind}: {hope.to_json(indent=2)}")
+        print("-" * 40)
+
+#    print(f"History 0: {history[0].to_json(indent=2)}")
+#    print("-" * 40)
+#    print(f"Future 0: {future[0].to_json(indent=2)}")
+#    print("-" * 40)
+
+
+### fucker test of combining treee fuckkkkk
+#    history[0].reverse()  # Reverse the first node
+#    history[0].update(future[0])  # Update it with the first future node
+#    freedom = history[0]
+#    print(f"Combined history and future: {freedom.to_json(indent=2)}")
+###    
+
+
+
+
+
+
+
+    for regret,hope in list(zip(history,future)):
         print("current",counter)
         counter+=1
+        regret.reverse()### fucking reverse it!!!
         if freedom:
             regret.giveback_child(freedom)
-#        print("--" * 40)
-#        print("regret", regret.to_json(indent=2))
-#        print("--" * 40)
-#        print("hope", hope.to_json(indent=2))
-        for child in regret.children:
-            print(child.to_json(indent=2))
+            regret.new_context_child(metadata={})## add glue back
+#       print("--" * 40)
+#       print("regret", regret.to_json(indent=2))
+#       print("--" * 40)
+#       print("hope", hope.to_json(indent=2))
+#        for child in regret.children:
+#            print(child.to_json(indent=2))
         regret.update(hope)
         freedom = regret
         print("current freedom", freedom.to_json(indent=2))
-    #print(freedom.to_json(indent=2))
+        print("-" * 40)
+#    print(freedom.to_json(indent=2))
+
